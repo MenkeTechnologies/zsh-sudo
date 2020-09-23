aa8943c61f8d1e3c34d2b85b6707807eb0d60ddd
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-23 16:04:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zsh-sudo
+# created by MenkeTechnologies
---
+++ sudo.plugin.zsh
@@ -0,0 +1,55 @@
+#!/usr/bin/env zsh
+#{{{                    MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Wed Sep 23 16:00:38 EDT 2020
+##### Purpose: zsh script to add sudo back to line
+##### Notes: 
+#}}}***********************************************************
+if [[ -z "$ZPWR_SUDO_CMD" ]]; then
+    export ZPWR_SUDO_CMD='sudo -E'
+    
+fi
+
+sudo-command-line() {
+    [[ -z $BUFFER ]] && LBUFFER="$(fc -ln -1)"
+
+    # Save beginning space
+    local WHITESPACE=""
+    if [[ ${LBUFFER:0:1} == " " ]] ; then 
+        WHITESPACE=" "
+        LBUFFER="${LBUFFER:1}"
+    fi
+
+    if [[ -n $EDITOR && $BUFFER == $EDITOR\ * ]]; then
+        if (( ${#LBUFFER} <= ${#EDITOR} )); then
+            RBUFFER=" ${BUFFER#$EDITOR }"
+            LBUFFER="sudoedit"
+        else
+            LBUFFER="sudoedit ${LBUFFER#$EDITOR }"
+        fi
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+        if (( ${#LBUFFER} <= 8 )); then
+            RBUFFER=" ${BUFFER#sudoedit }"
+            LBUFFER="$EDITOR"
+        else
+            LBUFFER="$EDITOR ${LBUFFER#sudoedit }"
+        fi
+    elif [[ $BUFFER == 'sudo '* ]]; then
+        if (( ${#LBUFFER} <= 4 )); then
+            RBUFFER="${BUFFER#$ZPWR_SUDO_CMD }"
+            LBUFFER=""
+        else
+            LBUFFER="${LBUFFER#$ZPWR_SUDO_CMD }"
+        fi
+    else
+        LBUFFER="$ZPWR_SUDO_CMD $LBUFFER"
+    fi
+
+    # Preserve beginning space
+    LBUFFER="${WHITESPACE}${LBUFFER}"
+}
+
+zle -N sudo-command-line
+
