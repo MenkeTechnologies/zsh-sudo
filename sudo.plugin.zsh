32adc1698c1a1690babd07a70a3e0d51040f9bed
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-24 00:10:46 -0400

    assignment covered

--- sudo.plugin.zsh
+++ sudo.plugin.zsh
@@ -19,13 +19,13 @@ sudo-command-line() {
     [[ -z "$BUFFER" ]] && LBUFFER="$(builtin fc -ln -1)"
 
     # notice in this regex how 1 or more required sudo
-    if [[ $LBUFFER =~ ^([[:space:]]*)(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*"$ZPWR_SUDO_REGEX"[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+(.*)$ ]]; then
+    if [[ $LBUFFER =~ ^([[:space:]]*)([[:graph:]]+=[[:graph:]]+[[:space:]]+)*(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*"$ZPWR_SUDO_REGEX"[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:graph:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:graph:]]+[[:space:]]+|--)*)*)+([[:space:]])*(.*)$ ]]; then
         # sudo wiith all args
         # white space before sudo and cmd
-        LBUFFER="$match[1]$match[12]"
+        LBUFFER="$match[1]$match[14]"
     # notice in this regex how 1 or more required sudo
-    elif [[ $RBUFFER =~ ^([[:space:]]*)(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*"$ZPWR_SUDO_REGEX"[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+(.*)$ ]]; then
-        RBUFFER="$match[1]$match[12]"
+    elif [[ $RBUFFER =~ ^([[:space:]]*)([[:graph:]]+=[[:graph:]]+[[:space:]]+)*(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*"$ZPWR_SUDO_REGEX"[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:graph:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:graph:]]+[[:space:]]+|--)*)*)+([[:space:]])*(.*)$ ]]; then
+        RBUFFER="$match[1]$match[14]"
     else
         LBUFFER="$ZPWR_SUDO_CMD $LBUFFER"
     fi
