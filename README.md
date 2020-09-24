750417da28f28ed1f0a067861237e02118c99924
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-23 22:53:27 -0400

    README and ability to use any string other than sudo

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # zsh-sudo
+
+## Functionality
+If sudo is present on current line it will be removed.  If sudo is not present it will be prepeneded to current line.  If current line is empty then previous command line will be prepended with sudo.
+
+## Prepend some other sstring
+You can have any string prepended instead of sudo like so:
+```sh
+export ZPWR_SUDO_REGEX='<mystr>'
+export ZPWR_SUDO_CMD='<mystr>'
+```
+These environment variables should have have the same string.
+
+## Keybinding this ZLE widget.
+
+```sh
+bindkey '^N' sudo-command-line
+```
+
 # created by MenkeTechnologies
+
--- sudo.plugin.zsh
+++ sudo.plugin.zsh
@@ -11,14 +11,20 @@ if ! (( $+ZPWR_SUDO_CMD )); then
     export ZPWR_SUDO_CMD='sudo'
 fi
 
+if ! (( $+ZPWR_SUDO_REGEX )); then
+    export ZPWR_SUDO_REGEX='sudo'
+fi
+
 sudo-command-line() {
     [[ -z "$BUFFER" ]] && LBUFFER="$(builtin fc -ln -1)"
 
-    if [[ $LBUFFER =~ ^([[:space:]]*)(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*sudo[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+(.*)$ ]]; then
+    # notice in this regex how 1 or more required sudo
+    if [[ $LBUFFER =~ ^([[:space:]]*)(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*"$ZPWR_SUDO_REGEX"[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+(.*)$ ]]; then
         # sudo wiith all args
         # white space before sudo and cmd
         LBUFFER="$match[1]$match[12]"
-    elif [[ $RBUFFER =~ ^([[:space:]]*)(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*sudo[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+(.*)$ ]]; then
+    # notice in this regex how 1 or more required sudo
+    elif [[ $RBUFFER =~ ^([[:space:]]*)(([\\\"\']*builtin[\\\"\']*[[:space:]]+)*[\\\"\']*command[\\\"\']*)?([[:space:]]*)(([\\\"\']*"$ZPWR_SUDO_REGEX"[\\\"\']*([[:space:]]+)((-[ABbEHnPSis]+[[:space:]]*|-[CghpTu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+([\\\"\']*env[\\\"\']*[[:space:]]+(-[iv]+[[:space:]]*|-[PSu][[:space:]=]+[[:alpha:]]+[[:space:]]+|--)*)*)+(.*)$ ]]; then
         RBUFFER="$match[1]$match[12]"
     else
         LBUFFER="$ZPWR_SUDO_CMD $LBUFFER"
